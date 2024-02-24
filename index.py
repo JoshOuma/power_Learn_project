4070db6a6cc823200de768d404c50c2285434cc1
JoshOuma <136029030+JoshOuma@users.noreply.github.com>
2024-02-24 08:34:19 -0800

    Create index.py

---
+++ index.py
@@ -0,0 +1,4 @@
+name=str(input("Whats your name "))
+age=str(input("Whats your age "))
+location=str(input("Whats your current location "))
+print("Hello {} your {} years old and you live in {}".format(name,age,location))
